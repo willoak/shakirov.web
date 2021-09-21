6a172a7682ad7aeb08d2fdc7e426dbbb7500060c
Willian Carvalho <Willss_oak@hotmail.com>
2021-09-20 23:46:07 -0300

    ajustes no javascript e css

--- js/script.js
+++ js/script.js
@@ -1,23 +1,18 @@
 let listWrapper = document.querySelector('.list-wrapper')
-let listItem = document.querySelectorAll('.list-item')
+let listItem = listWrapper.querySelectorAll('.list-item')
 let prevBtn  = document.querySelector('#prev')
 let nextBtn = document.querySelector('#next')
 let countItem = 0;
 let valorSlide = 0;
 const listITemWidth = listItem[0].offsetWidth
 const plusSize = 20;
-let listWrapperWidth = ( (listITemWidth + plusSize ) * listItem.length)
+let listWrapperWidth = ((listITemWidth + plusSize ) * listItem.length)
 
 
-function next(e){
-    e.preventDefault();
-    removeActiveClass()
-
+function next(){
     valorSlide += listITemWidth + plusSize
-
-    addActiveClass(++countItem)
-
     if(valorSlide <= (listWrapperWidth - listITemWidth) ) {
+        addActiveClass(++countItem)
         listWrapper.style.marginLeft = -valorSlide + 'px'
     } else {
         listWrapper.style.marginLeft = 0
@@ -27,15 +22,19 @@ function next(e){
     }
 }
 
-function prev(e){
-    e.preventDefault();
+function prev(){
     if(valorSlide > 0) {
-        removeActiveClass()
         addActiveClass(--countItem)
         valorSlide -= listITemWidth + plusSize
         listWrapper.style.marginLeft = -valorSlide + 'px'
     }
+}
 
+function addActiveClass(item){
+    listItem.forEach( item => item.classList.remove('active'))
+    if (item < (listItem.length ) ) {
+        listItem[item].classList.add('active')
+    }
 }
 
 function init(){
@@ -43,19 +42,6 @@ function init(){
     listItem[0].classList.add('active')
     nextBtn.addEventListener('click', next)
     prevBtn.addEventListener('click', prev)
-}
-
-function removeActiveClass(){
-    listItem.forEach( item => {
-        item.classList.remove('active')
-    })
-}
-
-function addActiveClass(item){
-    console.log(item)
-    if (item < (listItem.length ) ) {
-        listItem[item].classList.add('active')
-    }
 }
 
 init()
